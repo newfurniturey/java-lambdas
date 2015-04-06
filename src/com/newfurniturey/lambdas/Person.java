baeaf7b50c8c9d1dc2a72147aebeaa61139fb7c4
Joey Fowler <newfurniturey@gmail.com>
2015-04-05 22:02:43 -0400

    base 'person' class

---
+++ src/com/newfurniturey/lambdas/Person.java
@@ -0,0 +1,59 @@
+package com.newfurniturey.lambdas;
+
+class Person {
+	private String surName;
+	private Gender gender;
+	private int age;
+
+	public static class Builder {
+		private int age = 0;
+		private Gender gender = Gender.MALE;
+		private String surName = "";
+
+		public Person.Builder age(int val) {
+			age = val;
+			return this;
+		}
+
+		public Person.Builder gender(Gender val) {
+			gender = val;
+			return this;
+		}
+
+		public Person.Builder surName(String surName) {
+			this.surName = surName;
+			return this;
+		}
+
+		public Person build() {
+			return new Person(this);
+		}
+	}
+
+	private Person(Person.Builder builder) {
+		surName = builder.surName;
+		age = builder.age;
+		gender = builder.gender;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+	
+	public String getSurName() {
+		return surName;
+	}
+
+	@Override public String toString() {
+		return String.format("Name: %s\nGender: %s\nAge: %d\n",
+			surName,
+			gender,
+			age
+		);
+	}
+
+}
