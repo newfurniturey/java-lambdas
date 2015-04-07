78b9faa4b4f0df8f12f8e2e66b10398fddf70d78
Joey Fowler <newfurniturey@gmail.com>
2015-04-06 20:55:49 -0400

    sort desc via inner class

--- src/com/newfurniturey/lambdas/ComparatorTest.java
+++ src/com/newfurniturey/lambdas/ComparatorTest.java
@@ -10,17 +10,31 @@ public class ComparatorTest {
 	public static void main(String[] args) {
 		List<Person> personList = PersonConfig.createList();
 		
-		// inner class
+		// inner class - sort ASC
 		Collections.sort(personList, new Comparator<Person>() {
 			public int compare(Person p1, Person p2) {
 				return p1.getSurName().compareTo(p2.getSurName());
 			}
 		});
 		
-		System.out.println("=== Sorted ASC SurName ===");
+		System.out.println("=== [inner] Sorted ASC SurName ===");
 		for (Person p : personList) {
 			System.out.println(p.getSurName());
 		}
+		
+		// inner class - sort DESC
+		Collections.sort(personList, new Comparator<Person>() {
+			public int compare(Person p1, Person p2) {
+				return p2.getSurName().compareTo(p1.getSurName());
+			}
+		});
+		
+		System.out.println("=== [inner] Sorted DESC SurName ===");
+		for (Person p : personList) {
+			System.out.println(p.getSurName());
+		}
+		
+		
 	}
 	
 }
