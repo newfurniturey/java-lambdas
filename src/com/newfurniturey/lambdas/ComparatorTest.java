424c48f21a3a81a6fb166ce646a66bd70e671e07
Joey Fowler <newfurniturey@gmail.com>
2015-04-06 20:57:59 -0400

    use lambdas to sort ASC/DESC

--- src/com/newfurniturey/lambdas/ComparatorTest.java
+++ src/com/newfurniturey/lambdas/ComparatorTest.java
@@ -11,29 +11,40 @@ public class ComparatorTest {
 		List<Person> personList = PersonConfig.createList();
 		
 		// inner class - sort ASC
+		System.out.println("=== [inner] Sorted ASC SurName ===");
 		Collections.sort(personList, new Comparator<Person>() {
 			public int compare(Person p1, Person p2) {
 				return p1.getSurName().compareTo(p2.getSurName());
 			}
 		});
-		
-		System.out.println("=== [inner] Sorted ASC SurName ===");
 		for (Person p : personList) {
 			System.out.println(p.getSurName());
 		}
 		
 		// inner class - sort DESC
+		System.out.println("=== [inner] Sorted DESC SurName ===");
 		Collections.sort(personList, new Comparator<Person>() {
 			public int compare(Person p1, Person p2) {
 				return p2.getSurName().compareTo(p1.getSurName());
 			}
 		});
-		
-		System.out.println("=== [inner] Sorted DESC SurName ===");
 		for (Person p : personList) {
 			System.out.println(p.getSurName());
 		}
 		
+		// lambda - sort ASC
+		System.out.println("=== [lambda] Sorted ASC SurName ===");
+		Collections.sort(personList, (p1, p2) -> p1.getSurName().compareTo(p2.getSurName()));
+		for (Person p : personList) {
+			System.out.println(p.getSurName());
+		}
+		
+		// lambda - sort DESC
+		System.out.println("=== [lambda] Sorted ASC SurName ===");
+		Collections.sort(personList, (p1, p2) -> p2.getSurName().compareTo(p1.getSurName()));
+		for (Person p : personList) {
+			System.out.println(p.getSurName());
+		}
 		
 	}
 	
